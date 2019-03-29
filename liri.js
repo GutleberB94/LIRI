e327833ba86cd5b8105be2d731c62ef27c51ccc3
BrendanG <gutleberb@gmail.com>
2019-03-29 16:38:13 -0500

    setup switch statement

--- liri.js
+++ liri.js
@@ -1,3 +1,46 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var moment = require("moment");
+var axios = require("axios");
+
+var userCommand = proccess.argv[2];
+var userSearch = process.argv.slice(3).join(" ");
+
+var spotify = new Spotify(keys.spotify);
+
+
+switch(userCommand) {
+
+    case "concert-this":
+
+    // code here
+
+    break;
+
+    case "spotify-this-song":
+
+    // code here
+
+    break;
+
+    case "movie-this":
+
+    // code here
+
+    break;
+
+    case "do-what-it-says":
+
+    // code here
+
+    break;
+
+    default:
+    console.log("Not a valid command valid commands are: ")
+    console.log("concert-this")
+    console.log("spotify-this-song")
+    console.log("movie-this")
+    console.log("do-what-it-says")
+}
+
