cfaf0fc10bb15cfbeeb84b469cf9e804897e4492
BrendanG <gutleberb@gmail.com>
2019-03-29 17:10:01 -0500

    fixed band search function

--- liri.js
+++ liri.js
@@ -11,51 +11,66 @@ var userSearch = process.argv.slice(3).join(" ");
 //var spotify = new Spotify(keys.spotify);
 
 
-switch(userCommand) {
+switch (userCommand) {
 
     case "concert-this":
 
-    // code here
+        getConcert(userSearch);
 
-    break;
+        break;
 
     case "spotify-this-song":
 
-    // code here
+        // code here
 
-    break;
+        break;
 
     case "movie-this":
 
-    // code here
+        // code here
 
-    break;
+        break;
 
     case "do-what-it-says":
 
-    // code here
+        // code here
 
-    break;
+        break;
 
     default:
-    console.log(userCommand + " is not a valid command, valid commands are: ")
-    console.log("concert-this")
-    console.log("spotify-this-song")
-    console.log("movie-this")
-    console.log("do-what-it-says")
+        console.log(userCommand + " is not a valid command, valid commands are: ")
+        console.log("concert-this")
+        console.log("spotify-this-song")
+        console.log("movie-this")
+        console.log("do-what-it-says")
 }
+
+console.log(userCommand)
+console.log(userSearch)
 
 
 function getConcert(userSearch) {
 
-    var artist = userSearchl;
+    var artist = userSearch;
 
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=trilogy")
-    .then(function(response) {
+        .then(function (response) {
 
-        console.log(response);
+            console.log(response);
 
 
 
-    }); // axios close
+        }) // axios close
+        .catch(function (error) {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 } // function close
